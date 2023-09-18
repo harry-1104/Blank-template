316b9c095dc54801e4ed54999d04e61c7ae96077
harry minh <quannguyenminhbl@gmail.com>
2023-09-18 00:17:02 +0000

    nothing

---
+++ start-ubuntu.sh
@@ -0,0 +1,10 @@
+clear
+echo "[+] Starting ubuntu ..."
+sudo docker run --name=ubuntu-gnome --rm \
+  --tmpfs /run --tmpfs /run/lock --tmpfs /tmp \
+  --cgroupns=host --cap-add SYS_BOOT --cap-add SYS_ADMIN \
+  -v /sys/fs/cgroup:/sys/fs/cgroup \
+  -p 5901:5901 -p 6901:6901 \
+  darkdragon001/ubuntu-gnome-vnc
+sudo docker rmi darkdragon001/ubuntu-gnome-vnc
+clear
